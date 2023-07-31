5ce95d091cc9e137360360bd63454cdc5fbaf78a
Oliver Vinneras <ov5800@rit.edu>
2023-07-31 14:25:52 +0200

    Small Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}","/index.html"],
+  content: ["./src/**/*.{html,js}","index.html"],
   theme: {
     extend: {},
   },
@@ -9,4 +9,3 @@ module.exports = {
     require('@tailwindcss/typography'),
   ],
 }
-
